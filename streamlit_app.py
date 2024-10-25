a6ce09dae866a9efb50f07f03dd5f9f4fc44a250
erika-gutierrez <erikacloa@ibm.com>
2024-10-25 19:24:32 +0800

    Update streamlit_app.py
    add fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,6 +58,9 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
-
+#New section to display fuirtyvice nutrition information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
 
 
